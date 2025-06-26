3eaa9a3a5cdfbe827267d5f876d043eb862ffd4d
resenhandodirei <larimscorrea@gmail.com>
2025-06-26 16:15:00 -0300

    refactor: atualizei as migrations do prisma conforme a alteração da coluna do banco de dados

--- src/prisma/migrations/20250626191152_init/migration.sql
+++ src/prisma/migrations/20250626191152_init/migration.sql
@@ -1,12 +1,12 @@
 -- CreateEnum
-CREATE TYPE "cargo_enum" AS ENUM ('estagiario', 'adm', 'defensor', 'assessor_juridico', 'psicossocial');
+CREATE TYPE "TipoDePerfilEnum" AS ENUM ('adm', 'defensor', 'servidor', 'psicossocial', 'estagiario');
 
 -- CreateTable
 CREATE TABLE "login" (
     "id" SERIAL NOT NULL,
     "nome" VARCHAR(100) NOT NULL,
     "genero" VARCHAR(20),
-    "cargo" "cargo_enum" NOT NULL,
+    "tipo_de_perfil" "TipoDePerfilEnum" NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "senha" VARCHAR(255) NOT NULL,
 
@@ -15,4 +15,3 @@ CREATE TABLE "login" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "login_email_key" ON "login"("email");
-
