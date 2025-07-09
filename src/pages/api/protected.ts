7d36882c326ebb5aeecd4e8601b6434811a8f4d2
resenhandodirei <larimscorrea@gmail.com>
2025-07-09 15:54:38 -0300

    feat: falta automatizar isso no Insomnia para que ele injete o token automaticamente após o login

--- src/pages/api/protected.ts
+++ src/pages/api/protected.ts
@@ -1,11 +1,12 @@
-// src/app/pages/api/protected.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { withAuth } from '../utils/withAuth';
+import { withAuth } from '../api/utils/withAuth';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   // @ts-ignore
   const user = req.user;
-  res.status(200).json({ message: `Olá, usuário de ID ${user.id}!` });
+  return res.status(200).json({
+    message: `Olá, usuário logado! Seu ID é ${user.id} e seu e-mail é ${user.email}.`,
+  });
 }
 
 export default withAuth(handler);
