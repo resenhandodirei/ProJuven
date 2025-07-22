7497cd09c9a095f0ac2c3efbaef0afdd71f3be7c
resenhandodirei <larimscorrea@gmail.com>
2025-07-22 16:33:49 -0300

    feat: gambiarra com tailwind cdn como componente

---
+++ src/components/tailwindjs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TailwindJS = () => {
+  return (
+    <>
+      <head>
+        <meta />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      </head>
+    </>
+  );
+};
+
+export default TailwindJS;
